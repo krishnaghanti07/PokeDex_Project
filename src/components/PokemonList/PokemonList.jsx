0bf2e9801e7ed1ba407571421dd06675ab11de59
Srikrishna Ghanti <nandini.n10s05@gmail.com>
2024-07-25 14:13:58 +0530

    Cleaned Up The States

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -5,24 +5,42 @@ import Pokemon from '../Pokemon/Pokemon';
 
 function PokemonList () {
 
-    const [pokemonList , setPokemonList] = useState([]) ;
-    const [isLoading , setIsLoading] = useState(true);
+    // const [pokemonList , setPokemonList] = useState([]) ;
+    // const [isLoading , setIsLoading] = useState(true);
 
-    const [pokedexUrl , setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon') ;
+    // const [pokedexUrl , setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon') ;
 
-    const [nextUrl , setNextUrl] = useState('');
-    const [prevUrl , setPrevUrl] = useState('');
+    // const [nextUrl , setNextUrl] = useState('');
+    // const [prevUrl , setPrevUrl] = useState('');
+
+    // We've to use This Single useState everywhere as per our Requirment ...
+    const [pokemonListState , setPokemonListState] = useState ({
+        pokemonList: [] ,
+        isLoading: true , 
+        pokedexUrl: 'https://pokeapi.co/api/v2/pokemon' ,
+        nextUrl: '' ,
+        prevUrl: '' ,
+    });
 
     async function downloadPokemons () {
-        setIsLoading(true);
+        // setIsLoading(true);
+        setPokemonListState ((state) => ({
+            ...state , 
+            isLoading: true
+        }));
 
-        const response = await axios.get (pokedexUrl); // This Downloads List of 20 Pokemons ...
+        const response = await axios.get (pokemonListState.pokedexUrl); // This Downloads List of 20 Pokemons ...
 
         const pokemonResults = response.data.results ; // We get the array of Pokemons from Results
         // console.log (response.data);
 
-        setNextUrl(response.data.next);
-        setPrevUrl(response.data.previous);
+        setPokemonListState ((state) => ({
+            ...state , 
+            nextUrl: response.data.next , 
+            prevUrl: response.data.previous
+        }));
+        // setNextUrl(response.data.next);
+        // setPrevUrl(response.data.previous);
 
         // Iterating over the Array of 'Pokemons' , and using their url , to create an Array of Promises
         // That will Download those 20 Pokemons
@@ -44,26 +62,31 @@ function PokemonList () {
             }
         });
         console.log (pokiListResult) ;
-        setPokemonList(pokiListResult);
-        setIsLoading (false);
+        setPokemonListState ((state) => ({
+            ...state ,
+            pokemonList: pokiListResult ,
+            isLoading: false ,
+        }));
+        // setPokemonList(pokiListResult);
+        // setIsLoading (false);
     }
 
     useEffect ( () => {
         downloadPokemons() ;
-    } , [pokedexUrl]);
+    } , [pokemonListState.pokedexUrl]);
 
     return (
         <>
             <div className="pokemon-list-wrapper">
                 <div>Pokemon List :</div>
                 <div className='pokemon-wrapper'>
-                    {(isLoading) ? "Loading ...." : 
-                        pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)
+                    {(pokemonListState.isLoading) ? "Loading ...." : 
+                        pokemonListState.pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)
                     }
                 </div>
                 <div className='controls'>
-                    <button disabled={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)} >Prev</button>
-                    <button disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)} >Next</button>
+                    <button disabled={pokemonListState.prevUrl == null} onClick={() => setPokemonListState({...pokemonListState , pokedexUrl: pokemonListState.prevUrl})} >Prev</button>
+                    <button disabled={pokemonListState.nextUrl == null} onClick={() => setPokemonListState({...pokemonListState , pokedexUrl: pokemonListState.nextUrl})} >Next</button>
                 </div>
             </div>
         </>
